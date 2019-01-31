0d7ae7cebc491e14dd367d0d5a88e20a972a6d3e
ambroisernd <ambroise.renaud@gmail.com>
2019-01-31 10:26:06 +0100

    initial commit

---
+++ core/Empty.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ambroise Renaud on 2019-01-31.
+//
+
+#include "Empty.h"
---
+++ fuzzy/Empty.h
@@ -0,0 +1,14 @@
+//
+// Created by Ambroise Renaud on 2019-01-31.
+//
+
+#ifndef FUZZY_EMPTY_H
+#define FUZZY_EMPTY_H
+
+
+class Empty {
+
+};
+
+
+#endif //FUZZY_EMPTY_H
