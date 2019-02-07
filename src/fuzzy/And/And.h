3d98234f31bcdc7c0f85a94f5632cca600db88fd
Paul Lemettre <paul.lemettre@uha.fr>
2019-02-07 21:06:06 +0100

    AndMin

---
+++ src/fuzzy/And/And.h
@@ -0,0 +1,13 @@
+#ifndef FUZZY_AND_H
+#define FUZZY_AND_H
+
+#include "BinaryExpression"
+
+namespace fuzzy {
+
+    class And : public core::BinaryExpression{
+
+    };
+
+}
+#endif FUZZY_AND_H
---
+++ src/fuzzy/And/AndMin.h
@@ -0,0 +1,20 @@
+#ifndef FUZZY_ANDMIN_H
+#define FUZZY_ANDMIN_H
+
+#include "And.h"
+#include "Expression.h"
+
+namespace fuzzy {
+    template <class T>
+    class AndMin : public And {
+    public:
+        virtual T evaluate(core::Expression, core::Expression);
+
+    };
+
+    template <class T>
+    T AndMin<T>::evaluate(core::Expression l, core::Expression r) {
+        return min(l , r);
+    }
+}
+#endif FUZZY_ANDMIN_H
