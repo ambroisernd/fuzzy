85461037b8786dec9b005337b0e66f2a275d99f1
ambroisernd <ambroise.renaud@gmail.com>
2019-02-08 10:26:57 +0100

    failed test

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #define ASSERT_ON
 #include "test.h"
-void dummyTest()
+#include "core/ValueModel.h"
+using namespace core;
+void valueModelTest()
 {
-    std::cout  << "dummy test" << std::endl;
-    ASSERT(1==1);
+    std::cout  << "set test" << std::endl;
+    ValueModel <int> vm = ValueModel<int>();
+    vm.setValue(5);
+    ASSERT(vm.evaluate() == 6);
 }
 
 int main() {
-    dummyTest();
-    std::cout << "Hello, World!" << std::endl;
+    valueModelTest();
     return 0;
 }
