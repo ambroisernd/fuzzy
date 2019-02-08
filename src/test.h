8cca57fa47d62b6dd5aff54d8ac37eaa88610cd2
ambroisernd <ambroise.renaud@gmail.com>
2019-02-08 08:53:02 +0100

    test

---
+++ src/test.h
@@ -0,0 +1,12 @@
+#ifndef ASSERT_ON
+#define ASSERT(x)
+#else
+#define ASSERT(x) \
+            if (!(x)) \
+                {\
+                std::cout << std ::endl << "ASSERT ERROR "<< #x << " failed" << std::endl; \
+                std::cout << "on line " << __LINE__ << std::endl; \
+                std::cout << "in file " << __FILE__ << std::endl; \
+                exit(EXIT_FAILURE);\
+                }
+#endif
