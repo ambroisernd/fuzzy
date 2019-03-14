f6c62b06df95cc7357c4fe458abc4f6621788b5b
ambroisernd <ambroise.renaud@gmail.com>
2019-03-14 09:46:45 +0100

    mamdani defuzz

--- src/fuzzy/Defuzz/MamdaniDefuzz.h
+++ src/fuzzy/Defuzz/MamdaniDefuzz.h
@@ -2,16 +2,29 @@
 #define FUZZY_MAMDANIDEFUZZ_H
 
 #include "../../core/BinaryExpression.h"
+#include "../Evaluator.h"
 
 namespace fuzzy{
     template <typename T>
     class MamdaniDefuzz : core::BinaryExpression<T>{
     private:
+        T min;
+        T max;
+        T step;
 
     public:
+        MamdaniDefuzz(T min, T max, T step);
         virtual T evaluate(core::Expression<T>* l, core::Expression<T>* r);
-        virtual T defuzz();
+        virtual T defuzz(typename Evaluator<T>::Shape) const = 0;
     };
+
+    template<typename T>
+    T MamdaniDefuzz<T>::evaluate(core::Expression<T> *l, core::Expression<T> *r) {
+        return defuzz(Evaluator<T>::BuildShape(min, max, step, l, r));
+    }
+
+    template<typename T>
+    MamdaniDefuzz<T>::MamdaniDefuzz(T min, T max, T step):min(min), max(max), step(step) {}
 
 }
 #endif //FUZZY_MAMDANIDEFUZZ_H
