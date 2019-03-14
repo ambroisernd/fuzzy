1e8af825bbf87575d158b5bd04f0342a2644db26
ambroisernd <ambroise.renaud@gmail.com>
2019-03-14 08:48:27 +0100

    template

---
+++ src/fuzzy/Defuzz/MamdaniDefuzz.h
@@ -0,0 +1,14 @@
+#ifndef FUZZY_MAMDANIDEFUZZ_H
+#define FUZZY_MAMDANIDEFUZZ_H
+
+#include "../../core/BinaryExpression.h"
+
+namespace fuzzy{
+    template <typename T>
+    class MamdaniDefuzz : core::BinaryExpression<T>{
+    private:
+
+    public:
+    };
+}
+#endif //FUZZY_MAMDANIDEFUZZ_H
