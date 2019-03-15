51830bf53db17a1947c69a0d62354db7d70017c5
ambroisernd <ambroise.renaud@gmail.com>
2019-03-15 11:20:21 +0100

    typedef

--- src/fuzzy/Defuzz/CogDefuzz.h
+++ src/fuzzy/Defuzz/CogDefuzz.h
@@ -8,12 +8,30 @@ namespace fuzzy{
     class CogDefuzz : MamdaniDefuzz<T>{
     private:
 
-        T defuzz(typename Evaluator<T>::Shape shape) const {
-            return nullptr;
-        }
-
     public:
+
+        CogDefuzz(T min, T max, T step);
+
+        T defuzz(typename Evaluator<T>::Shape shape) const ;
     };
+
+    template<typename T>
+    T CogDefuzz<T>::defuzz(typename Evaluator<T>::Shape shape) const {
+        T fiber;
+        T image;
+        T num = 0 ;
+        T denom = 0;
+        for (int i = 0; i < shape.first.size() ; ++i) {
+            fiber = shape.first.at(i);
+            image = shape.second.at(i);
+            num += image*fiber;
+            denom += image;
+        }
+        return num/denom;
+    }
+
+    template<typename T>
+    CogDefuzz<T>::CogDefuzz(T min, T max, T step):MamdaniDefuzz<T>(min, max, step) {}
 }
 
 #endif //FUZZY_COGDEFUZZ_H
