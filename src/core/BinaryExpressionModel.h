579b3a2f6e46e65c84917465a34964ae05ff0bf1
ambroisernd <ambroise.renaud@gmail.com>
2019-03-15 12:47:55 +0100

    binary expression fix

--- src/core/BinaryExpressionModel.h
+++ src/core/BinaryExpressionModel.h
@@ -16,6 +16,20 @@ namespace core {
         Expression<T> right;
         BinaryExpression<T> operatore;
     };
+
+    template<typename T>
+    T BinaryExpressionModel<T>::evaluate() const {
+        if(operatore != nullptr){
+            return operatore.evaluate(left, right);
+        }
+    }
+
+    template<typename T>
+    T BinaryExpressionModel<T>::evaluate(Expression <T> *l, Expression <T> *r) const {
+        if (left !=nullptr && right!=nullptr) {
+            return evaluate(left, right);
+        }
+    }
 }
 
 #endif //FUZZY_BINARYEXPRESSIONMODEL_H
