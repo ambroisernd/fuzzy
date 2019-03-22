4abadcb4133db38289105828766aa0a7c8446d87
ambroisernd <ambroise.renaud@gmail.com>
2019-03-22 19:39:09 +0100

    sugenothen

--- src/fuzzy/fuzzy.h
+++ src/fuzzy/fuzzy.h
@@ -16,6 +16,7 @@
 #include "is/isTrapezeRight.h"
 #include "Defuzz/CogDefuzz.h"
 #include "Or/OrPlus.h"
+#include "Then/SugenoThen.h"
 
 typedef fuzzy::AggMax<NUMERIC> AggMax;
 typedef fuzzy::AndMin<NUMERIC> AndMin;
@@ -35,5 +36,6 @@ typedef fuzzy::CogDefuzz<NUMERIC> CogDefuzz;
 typedef fuzzy::FuzzyFactory<NUMERIC> FuzzyFactory;
 typedef fuzzy::AggPlus<NUMERIC> AggPlus;
 typedef fuzzy::OrPlus<NUMERIC> OrPlus;
+typedef fuzzy::SugenoThen<NUMERIC> SugenoThen;
 
 #endif //FUZZY_FUZZY_H
