2b19d6fb912a07ae8aa2865eab9b7faaa418331f
Paul <paul.lemettre77@gmail.com>
2019-03-23 23:03:48 +0100

    NaryShadowExpression (no test)

---
+++ src/core/NaryShadowExpression.h
@@ -0,0 +1,34 @@
+#ifndef FUZZY_NARYSHADOWEXPRESSION_H
+#define FUZZY_NARYSHADOWEXPRESSION_H
+
+#include "NaryExpression.h"
+
+namespace core {
+    template <typename T>
+    class NaryShadowExpression : public NaryExpression<T> {
+    public:
+        explicit NaryShadowExpression(NaryExpression <T> *target);
+        void setTarget(NaryExpression<T> *target);
+        virtual T evaluate(std::vector<core::Expression *> *o) const ;
+        //TODO getTarget
+
+    private:
+        NaryExpression<T> * target;
+
+    };
+    template<typename T>
+    T NaryShadowExpression<T>::evaluate(std::vector<core::Expression<T> *> *o) const {
+        if(target!= nullptr){
+            return target->evaluate(o);
+        }
+    }
+
+    template<typename T>
+    void NaryShadowExpression<T>::setTarget(NaryExpression<T> *target) {
+        NaryShadowExpression::target = target;
+    }
+
+    template<typename T>
+    NaryShadowExpression<T>::NaryShadowExpression(NaryExpression <T> *target):target(target) {}
+}
+#endif //FUZZY_NARYSHADOWEXPRESSION_H
