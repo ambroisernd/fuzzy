c0e1bbdef44819bad4ae654346457cb171f50557
ambroisernd <ambroise.renaud@gmail.com>
2019-04-24 14:09:45 +0200

    sugeno

--- src/fuzzy/Defuzz/SugenoDefuzz.h
+++ src/fuzzy/Defuzz/SugenoDefuzz.h
@@ -18,7 +18,7 @@ namespace fuzzy{
         T denominateur = 0;
         for(typename std::vector<core::Expression<T>*>::iterator it = o->begin();it != o->end();it++){
             T eval = (*it)->evaluate();
-            SugenoThen<T> sgThen = (**it);
+            SugenoThen<T> *sgThen = (SugenoThen<T>*)(*it);
             denominateur += sgThen->premiseValue();
             numerateur += eval;
 
